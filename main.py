720c42df5ea94f43887edaaba8a42d8bf7a051b1
Gustavo Bodra <gustavo.bodra@startse.com>
2024-01-28 16:58:38 -0300

    Add .code-workspace and .DS_Store to .gitignore

---
+++ main.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+from ultralytics.solutions import heatmap
+import cv2
+
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture("vids/Mall_2.mp4")
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+# Video writer
+video_writer = cv2.VideoWriter("Mall_2_heatmap.mp4",
+                               cv2.VideoWriter_fourcc(*'avc1'),
+                               fps,
+                               (w, h))
+
+# Init heatmap
+heatmap_obj = heatmap.Heatmap()
+heatmap_obj.set_args(colormap=cv2.COLORMAP_PARULA,
+                     imw=w,
+                     imh=h,
+                     view_img=True,
+                     shape="circle")
+
+while cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    tracks = model.track(im0, persist=True, show=False)
+
+    im0 = heatmap_obj.generate_heatmap(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
